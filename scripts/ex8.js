afa3755cc30770cbd35835e6e4610f08008dc6d5
Gabriel Henrique <l77gabrielgh77@hotmail.com>
2024-12-05 22:19:05 -0300

    commit by vscode

--- scripts/ex8.js
+++ scripts/ex8.js
@@ -1,4 +1,4 @@
 //Eventos DOM: Crie uma página HTML com um botão que, quando clicado, exiba um alerta que diga “Que demais!”.
 function alerta() {
     alert("Que demais!")
-} //Conlusão feita no exbase.html
+} //Conlusão feita no index.html
